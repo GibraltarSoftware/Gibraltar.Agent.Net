1c38e04415cf9305622eb971b52a0d63b69a02c2
Kendall Miller <kendall@gibraltarsoftware.com>
2013-05-21 00:55:05 -0400

    Initial checkin of working version

---
+++ src/Agent.Net.Test/SetupFixture.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Linq;
+using System.Reflection;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Gibraltar.Agent.Net.Test
+{
+    [TestClass]
+    public class SetupFixture
+    {
+        [AssemblyInitialize]
+        public static void Setup(TestContext context)
+        {
+            Log.Initializing += LogOnInitializing;
+            Log.StartSession("Starting Unit Tests");
+        }
+
+        [AssemblyCleanup]
+        public static void Shutdown()
+        {
+            Log.EndSession("Closing Unit Tests");
+        }
+
+        private static void LogOnInitializing(object sender, LogInitializingEventArgs e)
+        {
+            e.Configuration.Publisher.ProductName = "Loupe";
+            e.Configuration.Publisher.ApplicationName = "Network Agent Tests";
+
+            AssemblyFileVersionAttribute assemblyFileVersion = (AssemblyFileVersionAttribute)Assembly.GetAssembly(typeof(ConnectivityMonitor)).GetCustomAttributes(typeof(AssemblyFileVersionAttribute), false).FirstOrDefault();
+            e.Configuration.Publisher.ApplicationVersion = new Version(assemblyFileVersion.Version);
+        }
+    }
+}
---
+++ src/Agent.Net/Internal/ConnectivityEventMetric.cs
@@ -0,0 +1,26 @@
+﻿using Gibraltar.Agent.Metrics;
+
+namespace Gibraltar.Agent.Net.Internal
+{
+    /// <summary>
+    /// Creates an event metric for connectivity information
+    /// </summary>
+
+    [EventMetric("Loupe", ConnectivityMonitor.LogCategory, "Availability", 
+        Description = "Tracks the availability of an endpoint via TCP")]
+    internal class ConnectivityEventMetric
+    {
+        public ConnectivityEventMetric(string ipAddress, bool isAvailable)
+        {
+            IpAddress = ipAddress;
+            IsAvailable = isAvailable;
+        }
+
+        [EventMetricInstanceName]
+        public string IpAddress { get; private set; }
+
+        [EventMetricValue("isAvailable", SummaryFunction.Average, "%",Caption = "Is Available", 
+            Description = "Indicates of the connection is available", IsDefaultValue = true)]
+        public bool IsAvailable { get; private set; }
+    }
+}
